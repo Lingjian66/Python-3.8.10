6c63c4cb6b646c5166619eff7728740e11643476
邓立文 <664514687@qq.com>
2022-11-06 00:09:45 +0800

    input and variable

---
+++ Math Functions and Modules/Input.py
@@ -0,0 +1,4 @@
+#使用input（）函数获取用户输入
+account=input("请输入账户：")
+password=input("请输入密码：")
+print(account,password)
---
+++ Math Functions and Modules/Variable.py
@@ -0,0 +1,5 @@
+#变量：可以以字母、数字、下划线组合，但是不能以数字开头。
+name="Jack"
+age=20
+_adder="Guidian"
+print(name,age,_adder)
